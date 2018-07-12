d1acc2d9cdcea25f9e5bd4582c2c036ef2f92acb
flbraun <flbraun@WKS714.init.de>
2018-07-12 13:58:17 +0200

    Zweiter Testcase

---
+++ Object Repository/Object1.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Object1</name>
+   <tag></tag>
+   <elementGuidId>97161880-11ff-4453-9cd3-a06430136cab</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
